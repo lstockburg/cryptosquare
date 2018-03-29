a0c0f791a816c3bc44b27d5537abf9dc58ec22c3
Luke Stockburger and William Chu <william@>
2018-03-29 15:06:13 -0700

    add additional JS comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ function encode(array) {
   // Index is the squareroot of the array length, rounded down
   var index = Math.floor(Math.sqrt(array.length))
   var encodedString = "";
-  // Loop shifts nested loop one character over until it hits the index to prevent redundant characters
+  // Loop shifts nested loop one character over until it hits the index to prevent redundant characters in encodedString
   for (var x = 0; x < index; x++) {
-    // Updates encodedString with each letter based on the index
+    // Updates encodedString with letters spaced out by the index
     var i = x;
     for( i ; i < array.length; i+= index) {
     encodedString += array[i]
@@ -29,8 +29,7 @@ $(document).ready(function() {
     var userInput = $("#user-input").val().trim().toLowerCase();
     // Capture string as array only matching a-z characters using regex
     var lettersArray = userInput.match(/[a-z]/g);
-
-
+    // Show the encoded output
     $("#output").empty().text(encode(lettersArray));
   });
 });
