70e1940115f1a59eadb3a8ccc79aaadff949cda1
Luke Stockburger and William Chu <william@>
2018-03-29 14:50:52 -0700

    add JS comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 // Business Logic
 function encode(array) {
-  var arrayLength = array.length;
-  var index = Math.ceil(Math.sqrt(arrayLength))
+  // Index is the squareroot of the array length, rounded down
+  var index = Math.floor(Math.sqrt(array.length))
   var encodedString = "";
-
-  for(var i = 0; i < arrayLength; i+= index) {
+  // Loop shifts nested loop one character over until it hits the index to prevent redundant characters
+  for (var x = 0; x < index; x++) {
+    // Updates encodedString with each letter based on the index
+    var i = x;
+    for( i ; i < array.length; i+= index) {
     encodedString += array[i]
-    console.log(encodedString + "first loop");
+    }
   }
-  for(var i = 1; i < arrayLength; i+= index) {
-    encodedString += array[i]
-    console.log(encodedString + "second loop");
-  }
-
+  // Return array of encodedString broken into 5 character increments
+  var encodedStringArray = encodedString.match(/.{1,5}/g);
   return encodedString;
 };
 
@@ -20,10 +20,12 @@ function encode(array) {
 $(document).ready(function() {
   $("#user-input-form").submit(function(event) {
     event.preventDefault();
+    // Grab input from user form, trim whitespace, lowercase
     var userInput = $("#user-input").val().trim().toLowerCase();
+    // Capture string as array only matching a-z characters using regex
     var lettersArray = userInput.match(/[a-z]/g);
 
-    console.log(lettersArray);
+
     $("#output").empty().text(encode(lettersArray));
   });
 });
